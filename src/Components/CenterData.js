535c4127c7849a5f8f560d9b7e2f393dfa17be27
mukul98s <mymukul112@gmail.com>
2021-05-09 16:43:15 +0530

    Changed font styles

--- src/Components/CenterData.js
+++ src/Components/CenterData.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import moment from "moment";
 
 function CenterData({ address, name, sessions, pincode, fee, id }) {
   const [showSession, setShowSession] = useState(false);
@@ -7,9 +8,9 @@ function CenterData({ address, name, sessions, pincode, fee, id }) {
     <div className="centerData" key={id}>
       <div className="centerData__container">
         <h3>Center Name: {name}</h3>
-        <h3>Pincode: {pincode}</h3>
+        <h3 className={fee === "Free" ? "green" : "red"}>Fee: {fee}</h3>
         <p>Address: {address}</p>
-        <h4>Fee: {fee}</h4>
+        <p>Pincode: {pincode}</p>
       </div>
       <div className="centerData__sessions">
         {sessions.map((session) => {
@@ -19,10 +20,19 @@ function CenterData({ address, name, sessions, pincode, fee, id }) {
               key={session.session_id}
             >
               <div className="centerData__session__card">
-                <h4>Available Capacity: {session.available_capacity}</h4>
-                <p>Date: {session.date}</p>
-                <h4>Vaccine: {session.vaccine}</h4>
-                <h4>Minimum Age: {session.min_age_limit} Years</h4>
+                <h4>
+                  Available Capacity: <span>{session.available_capacity}</span>
+                </h4>
+                <h4>
+                  Date:{" "}
+                  <span>{moment(session.date).format("MMMM Do YYYY")}</span>
+                </h4>
+                <h4>
+                  Vaccine: <span>{session.vaccine}</span>
+                </h4>
+                <h4>
+                  Minimum Age: <span>{session.min_age_limit} Years</span>
+                </h4>
               </div>
             </div>
           );
@@ -31,6 +41,7 @@ function CenterData({ address, name, sessions, pincode, fee, id }) {
       <button
         className="centerData__toggle"
         onClick={() => setShowSession(!showSession)}
+        className="btn"
       >
         {!showSession ? "Show Sessions" : "Hide Sessions"}
       </button>
