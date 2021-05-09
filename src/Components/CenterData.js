3b9606fadb14b6d776dbfc6bbf751696e65aa31a
mukul98s <mymukul112@gmail.com>
2021-05-09 18:12:13 +0530

    Destructured Data within the file

--- src/Components/CenterData.js
+++ src/Components/CenterData.js
@@ -1,50 +1,56 @@
 import React, { useState } from "react";
 import moment from "moment";
 
-function CenterData({ address, name, sessions, pincode, fee, id }) {
+function CenterData({ center }) {
+  const { center_id, address, name, sessions, pincode, fee_type } = center;
   const [showSession, setShowSession] = useState(false);
 
   return (
-    <div className="centerData" key={id}>
-      <div className="centerData__container">
-        <h3>Center Name: {name}</h3>
-        <h3 className={fee === "Free" ? "green" : "red"}>Fee: {fee}</h3>
-        <p>Address: {address}</p>
-        <p>Pincode: {pincode}</p>
-      </div>
-      <div className="centerData__sessions">
-        {sessions.map((session) => {
-          return (
-            <div
-              className={`centerData__session ${showSession ? "show" : ""}`}
-              key={session.session_id}
-            >
-              <div className="centerData__session__card">
-                <h4>
-                  Available Capacity: <span>{session.available_capacity}</span>
-                </h4>
-                <h4>
-                  Date: <span>{session.date}</span>
-                </h4>
-                <h4>
-                  Vaccine: <span>{session.vaccine}</span>
-                </h4>
-                <h4>
-                  Minimum Age: <span>{session.min_age_limit} Years</span>
-                </h4>
+    <React.Fragment>
+      <section className="centerData" key={center_id}>
+        <div className="centerData__container">
+          <h3>Center Name: {name}</h3>
+          <h3 className={fee_type === "Free" ? "green" : "red"}>
+            Fee: {fee_type}
+          </h3>
+          <p>Address: {address}</p>
+          <p>Pincode: {pincode}</p>
+        </div>
+        <div className="centerData__sessions">
+          {sessions.map((session) => {
+            return (
+              <div
+                className={`centerData__session ${showSession ? "show" : ""}`}
+                key={session.session_id}
+              >
+                <div className="centerData__session__card">
+                  <h4>
+                    Available Capacity:{" "}
+                    <span>{session.available_capacity}</span>
+                  </h4>
+                  <h4>
+                    Date: <span>{session.date}</span>
+                  </h4>
+                  <h4>
+                    Vaccine: <span>{session.vaccine}</span>
+                  </h4>
+                  <h4>
+                    Minimum Age: <span>{session.min_age_limit} Years</span>
+                  </h4>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-      <button
-        className="centerData__toggle"
-        onClick={() => setShowSession(!showSession)}
-        className="btn"
-      >
-        {!showSession ? "Show Sessions" : "Hide Sessions"}
-      </button>
-    </div>
+            );
+          })}
+        </div>
+        <button
+          className="centerData__toggle"
+          onClick={() => setShowSession(!showSession)}
+          className="btn"
+        >
+          {!showSession ? "Show Sessions" : "Hide Sessions"}
+        </button>
+      </section>
+    </React.Fragment>
   );
 }
 
