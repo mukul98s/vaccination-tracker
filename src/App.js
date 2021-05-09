ac61660b1db3896ad8e0e7e5a8e28b4ac6f17c01
mukul98s <mymukul112@gmail.com>
2021-05-09 19:06:59 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import Search from "./Components/Search";
 
 function App() {
   return (
-    <div className="app">
-      <div className="app__title">
-        <h2>Search Vaccination Center</h2>
+    <React.Fragment>
+      <div className="app">
+        <div className="app__title">
+          <h2>Search Vaccination Center</h2>
+        </div>
+        <Search />
       </div>
-      <Search />
-    </div>
+
+      <footer>
+        <div className="section">
+          <h3>
+            Developed by{" "}
+            <a href="https://www.linkedin.com/in/mukul98s/" target="_blank">
+              Mukul Sharma
+            </a>
+          </h3>
+        </div>
+      </footer>
+    </React.Fragment>
   );
 }
 
